54f889d5d3fba579bd111943a56349512baf036c
jacso <dudin10@mail.ru>
2021-08-24 11:47:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/data/Weather.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.data
+
+data class Weather(var town: String = "my town", var temperature: Int = 10) {
+
+    constructor(town: String, temperature: Int, pressure: Int) : this(town, temperature) {
+        this.pressure = pressure
+    }
+
+    constructor(humidinty:Int):this(){}
+
+    var pressure:Int = 0
+}
---
+++ app/src/main/java/com/example/myapplication/repozitory/Repository.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication.repozitory
+
+import com.example.myapplication.data.Weather
+
+open class Repository : IRepository {
+    private val weathers: List<Weather> = listOf(
+        Weather("Moscow", 25),
+        Weather("Voronej", 5)
+    )
+
+    override fun getWeathers(): List<Weather> {
+        return weathers
+    }
+}
+
+interface IRepository {
+    fun getWeathers(): List<Weather>
+}
+
+object RepositorySingle : IRepository {
+    private val weathers: List<Weather> = listOf(
+        Weather("town", 25),
+        Weather("superTown", 5)
+    )
+
+    override fun getWeathers(): List<Weather> {
+        return weathers
+    }
+}
+
+inline fun getRepository(): IRepository {
+    return Repository()
+}
