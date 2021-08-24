9a996df6bd3cdf73c719cd37678907ef1bc47ddc
jacso <dudin10@mail.ru>
2021-08-24 13:34:33 +0300

    повесил заглушку на кнопку

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.R
 import com.example.myapplication.repozitory.RepositorySingle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    val recyclerWeather = findViewById<RecyclerView>(R.id.resyclerWeather)
+
+        val recyclerWeather = findViewById<RecyclerView>(R.id.resyclerWeather)
         val adapter = WeatherAdapter(RepositorySingle)
-        recyclerWeather.adapter= adapter
+        recyclerWeather.adapter = adapter
+
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener { Toast.makeText(this,"кнопка",Toast.LENGTH_SHORT).show() }
     }
 }
